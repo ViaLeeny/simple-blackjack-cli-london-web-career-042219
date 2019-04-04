5f197631dfd813cdc7929c2c503dc200fbe0fcde
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-04 05:24:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,7 @@ def hit? (card_total)
   if user_input == 'h'
   deal_card += card_total
   elsif user_input == 's' 
-  return card_ total
+  return card_total
   else
   invalid_command
   prompt_user
