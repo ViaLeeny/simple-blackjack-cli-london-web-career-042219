46d253bba57e3d734db6087f78a7d03235d64c1a
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-04 05:16:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -22,11 +22,12 @@ def end_game (card_total)
 puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round ()
+def initial_round
  card1= deal_card 
  card2 = deal_card
   card_total = card1 + card2
   puts display_card_total(card_total)
+  return card_total
 end
 
 def hit? (card_total)
