3c7a00bf8b38d70dd6e082320803bf8c89588bf7
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-04 05:11:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,7 @@ puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round (deal_card)
- return deal_card +  deal_card
+ card_total= deal_card +  deal_card
   
   puts display_card_total
 end
