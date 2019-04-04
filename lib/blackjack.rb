21db54bab5d421f400226030922605229b219307
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-04 05:38:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,13 +23,12 @@ puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
- card1= deal_card 
- card2 = deal_card
-  card_total = card1 + card2
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total) 
   return card_total
-  prinyt display_card_total(card_total)
 end
-
 def hit? (card_total)
   prompt_user
   user_input = get_user_input
